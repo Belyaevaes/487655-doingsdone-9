5e4dd25c53a0b64742696d552256bd826ea63d5d
Екатерина <belyaevaes@gmail.com>
2019-05-01 20:08:25 +0300

    Работа с датами

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+define('SECS_IN_HOUR', 3600);
 
 function countProjectTasks(array $proj_tasks, $project_name) {
     $number = 0;
@@ -9,4 +10,13 @@ function countProjectTasks(array $proj_tasks, $project_name) {
         }
     }
     return $number;
+}
+
+function checkExpirationDate($date, $expirationHours = 24) {
+    $tsDate = strtotime($date);
+    $tsNow = time();
+    $tsDiff = $tsDate - $tsNow;
+    $hoursDiff = floor($tsDiff / SECS_IN_HOUR);
+
+    return ($hoursDiff < $expirationHours);
 }
